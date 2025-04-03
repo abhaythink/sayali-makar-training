06f79639e6270f23952369a1590646b73820556f
sayali.makar <sayali.makar@thinkitive.com>
2025-04-03 13:28:17 +0530

    Add new files for basics, data types, and objects

---
+++ Day1/Objects.js
@@ -0,0 +1,18 @@
+
+let array = new Array();
+console.log(array);
+
+let nums = Array(1,2,3);
+console.log(nums.length);
+
+let names = ['sayali','john','mac'];
+console.log(names);
+
+let fruits = ['apple','mango','grapes','banana'];
+console.log(fruits);
+console.log(fruits.indexOf('apple'));
+fruits.push('cherry');
+fruits.pop();
+fruits.unshift('orange');
+fruits.shift();
+console.log(fruits);
